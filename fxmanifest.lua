c56b395da7a10d96989c75d5bdb65b86a3bb3345
Max Michaels <max32268@gmail.com>
2023-04-20 19:09:29 -0500

    remove unused config

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,12 +5,10 @@ author 'Sonoran Software Systems'
 real_name 'Sonoran CMS Permissions'
 description 'Sonoran CMS to Ace permissions translation layer'
 version '1.1.0'
-git_repo 'https://github.com/Sonoran-Software/cms_ace_perms'
+git_repo 'https://github.com/Sonoran-Software/sonorancms_ace_perms'
 
 lua54 'yes'
 
 server_scripts {'server/server.lua', 'config.lua'}
-
-escrow_ignore {'config.CHANGEME.lua'}
 
 ui_page 'nui/index.html'
